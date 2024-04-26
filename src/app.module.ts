64aa28264b5ff596c6b7fa6fc962e84fbe24df34
Akash <akashdevcc@gmail.com>
2024-04-26 10:10:47 +0530

    feat: pinohttp genReqId used to generate unique request ID.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
+import { randomUUID } from 'crypto';
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: async () => {
+        const level = 'trace';
+        const isPretty = true;
+        return {
+          pinoHttp: {
+            level: level,
+            genReqId: function (req, res) {
+              const existingID = req.id ?? req.headers['x-request-id'];
+              if (existingID) return existingID;
+              const id = randomUUID();
+              res.setHeader('X-Request-Id', id);
+              return id;
+            },
+            transport: isPretty
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: false,
+                  },
+                }
+              : undefined,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
